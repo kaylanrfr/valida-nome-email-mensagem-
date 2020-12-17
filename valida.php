8a1f285f2da69c4bdb0a22f262e6ff5554309884
kaylanrfr <60267658+kaylanrfr@users.noreply.github.com>
2020-12-17 13:56:47 -0300

    Update valida.php

--- valida.php
+++ valida.php
@@ -23,51 +23,58 @@ if (isset($_POST["nome"]) and isset($_POST["e-mail"]) and isset($_POST["telefone
 	$nome = $_POST["nome"];
 	$telefone = $_POST["telefone"];
 	$mensagem = $_POST["mensagem"];
-	if (!filter_var($remetente, FILTER_VALIDATE_EMAIL)) {//valida o email
+	if (!filter_var($remetente, FILTER_VALIDATE_EMAIL)) { //valida o email
 		echo ("<script LANGUAGE='JavaScript'>
 	window.alert('O email " . $remetente . " escrito é inválido.');
 	window.history.back()
 	</script>");
-	} elseif (!preg_match('/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/', $nome)) {//valida nome só letras e espaços
+	} elseif (!preg_match('/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/', $nome)) { //valida nome só letras e espaços
 		echo ("<script LANGUAGE='JavaScript'>
 	window.alert('Preencha o nome sem caracteres especiais e numeros');
 	window.history.back()
 	</script>");
-	} elseif (strlen($nome) < 3) {//nome maior que 3
+	} elseif (strlen($nome) < 3) { //nome maior que 3
 		echo ("<script LANGUAGE='JavaScript'>
 	window.alert('Preencha o nome.');
 	window.history.back()
 	</script>");
-	} elseif (strlen($telefone) != 11) {//telefone no rj tem 11 numeros
+	} elseif (strlen($telefone) != 11) { //telefone no rj tem 11 numeros
 		echo ("<script LANGUAGE='JavaScript'>
 	window.alert('O número digitado é diferente do padrão. preencha com o ddd e o número.');
 	window.history.back()
 	</script>");
-	} elseif (!is_numeric($telefone)) {//verifica se só tem numeros
+	} elseif (!is_numeric($telefone)) { //verifica se só tem numeros
 		echo ("<script LANGUAGE='JavaScript'>
 	window.alert('O número digitado possui caracteres não numéricos. preencha com o ddd e o número.');
 	window.history.back()
 	</script>");
 	} elseif (strlen($mensagem) < 20) {;
 		echo ("<script LANGUAGE='JavaScript'>
-	window.alert('Preencha a mensagem.');
+	window.alert('Preencha a mensagem. Mínimo 20 caracteres.');
 	window.history.back()
 	</script>");
 	} else {
 		$ok = true;
 	}
-	$email = "cai16146@cuoly.com";//email do cliente
-	$titulo = "Contato IRM";
-	$msg = "Nome: " . $nome . "<br>
-    Email: " . $remetente . "<br>
-    Telefone: " . $telefone . "<br>
-    Mensagem: " . $mensagem . "<br>";
-	$confirma = enviarEmail($titulo, $email, $msg);
-	if ($confirma and $ok == true) {
-		echo ("<script LANGUAGE='JavaScript'>
+	if ($ok) {
+		$email = ""; //email do cliente
+		$titulo = "Contato IRM";
+		$msg = "Nome: " . $nome . "<br>
+    	Email: " . $remetente . "<br>
+    	Telefone: " . $telefone . "<br>
+		Mensagem: " . $mensagem . "<br>";
+		$confirma = enviarEmail($titulo, $email, $msg);
+		if ($confirma) {
+			echo ("<script LANGUAGE='JavaScript'>
     window.alert('Enviado com sucesso.');
     window.location.href='index.html';
 	</script>");
+		} else {
+			echo ("<script LANGUAGE='JavaScript'>
+    window.alert('Problema durante o envio.');
+    window.history.back()
+	</script>");
+		}
 	}
 } else {
 	echo ("<script LANGUAGE='JavaScript'>
